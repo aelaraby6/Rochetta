d220977a9e8ea305e7fb2762ef82c81d8948a4bf
aelaraby6 <body1262005@gmail.com>
2025-09-04 18:55:51 +0300

    Add Product Model

---
+++ Server/models/Product/product.model.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["Pain Relief", "Cold and Flu", "Diabetes Care", "First Aid"],
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    requires_prescription: {
+      type: Boolean,
+      default: false,
+    },
+    has_strips: {
+      type: Boolean,
+      default: false,
+    },
+    strip_count: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    is_deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
