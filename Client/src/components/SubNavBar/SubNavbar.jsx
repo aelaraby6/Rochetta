e30a01a41173d06b308b12e0f3cfa5c41110c8c8
aelaraby6 <body1262005@gmail.com>
2025-09-08 02:28:31 +0300

    Enhance Header Ui

--- Client/src/components/SubNavBar/SubNavbar.jsx
+++ Client/src/components/SubNavBar/SubNavbar.jsx
@@ -3,25 +3,25 @@ import "./SubNavbar.css";
 
 const SubNavbar = () => {
   return (
-    <div className="subnavbar">
-      <nav className="subnavbar-nav">
-        <div className="links-container">
-          <Link to="/category/pain-relief" className="link-box">
-            Pain Relief
-          </Link>
-          <Link to="/category/cold-and-flu" className="link-box">
-            Cold and Flu
-          </Link>
-          <Link to="/category/diabetes-care" className="link-box">
-            Diabetes Care
-          </Link>
-          <Link to="/category/first-aid" className="link-box">
-            First Aid
-          </Link>
-        </div>
-        <div className="shipping-box">Free Shipping Order By August</div>
-      </nav>
-    </div>
+    <nav className="subnavbar-nav">
+      <div className="links-container">
+        <Link to="/category/pain-relief" className="link-box">
+          Pain Relief
+        </Link>
+        <Link to="/category/cold-and-flu" className="link-box">
+          Cold and Flu
+        </Link>
+        <Link to="/category/diabetes-care" className="link-box">
+          Diabetes Care
+        </Link>
+        <Link to="/category/first-aid" className="link-box">
+          First Aid
+        </Link>
+      </div>
+      <div className="shipping-box">
+        Free Shipping Order By August
+      </div>
+    </nav>
   );
 };
 
