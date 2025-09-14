bb5f528a34c37d212ce11c464695d178b0510320
aelaraby6 <body1262005@gmail.com>
2025-09-14 14:21:05 +0300

    Add Order Model

---
+++ Server/models/Order/order.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: { type: Number, required: true, min: 1 },
+      price: { type: Number, required: true, min: 0 },
+    },
+  ],
+  total: { type: Number, required: true, min: 0 },
+  status: {
+    type: String,
+    enum: ["pending", "shipped", "delivered", "canceled"],
+    default: "pending",
+  },
+  is_deleted: { type: Boolean, default: false },
+});
+
+export const Order = mongoose.model("order", orderSchema);
