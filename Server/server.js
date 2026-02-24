1048e5344d79bfbfe478e25594dc4349088bdcff
Abdelrahman M Selim <engabdo880@gmail.com>
2026-02-24 08:30:20 +0200

    raedy for production

--- Server/server.js
+++ Server/server.js
@@ -5,9 +5,12 @@ import dbconnect from "./config/dbConnect.js";
 config();
 
 const port = process.env.PORT || 3000;
+const HOST = process.env.IP || "0.0.0.0";
 dbconnect()
   .then(() => {
-    app.listen(port, () => console.log("Server running on port " + port));
+    app.listen(port, HOST, () =>
+      console.log(`Server is running on http://${HOST}:${port}`),
+    );
   })
   .catch((err) => {
     console.error("Failed to connect DB or start server:", err.message);
